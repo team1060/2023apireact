78c559f77f8ab6b3ac2c5e13e1a78b7222aac571
team1060 <team1060dev@gmail.com>
2023-12-20 18:14:40 +0900

    commit

--- src/components/api.js
+++ src/components/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseURL = 'http://localhost:8081/api'
+const baseURL = 'http://localhost:8081'
 const axiosInstance = axios.create({
     baseURL,
     headers: {
